77060bd441a930ca9542cb91d522411dcdd51a1f
Oliver Hitchens <t9dt24wnj8@privaterelay.appleid.com>
2026-06-01 21:04:04 +1200

    edited file structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,3 @@
 import { defineConfig } from 'astro/config'
 
-export default defineConfig({
-  // use existing `site/` folder as the public directory so current asset paths (/assets/...) keep working
-  publicDir: 'site',
-})
+export default defineConfig({})
